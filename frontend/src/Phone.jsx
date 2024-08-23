a7ee1e63daee6f31e4dc0f422a97596c56a472cf
jaaskelainenL <jaaskelainenL@protonmail.com>
2024-08-23 22:39:42 +0300

    Add the most intuitive way to add your phone number

---
+++ frontend/src/Phone.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+
+function Phone(){
+
+
+    const [phone, setPhone] = useState("");
+
+    const [curPhoneBuf, setCurBuf] = useState("");
+    const [newNumBuf, setNewNumBuf] = useState(0);
+
+
+    const addNumber = () => {
+        setCurBuf((n)=>n+newNumBuf)
+        setPhone(curPhoneBuf);
+    }
+    const removeNumber = () => {
+        setCurBuf((n) => n.substring(0,n.length-1));
+        setPhone(curPhoneBuf);
+    };
+
+
+    useEffect(() => {
+        // Set up an interval to update newNum every second
+        const intervalId = setInterval(() => {
+            setNewNumBuf(prevNum => {
+                prevNum += 1
+                if(prevNum>=10){
+                    prevNum=0;
+                }
+
+                return prevNum;
+            }); // Increment newNum by 1 every second
+        }, 250);
+    
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
+      }, []);
+
+
+    return(
+        <>
+        
+        <div>
+            Phone number:
+            <input type="text" value={curPhoneBuf + newNumBuf} readOnly/>
+        </div>
+
+        <button type="button" onClick={addNumber}>Add!</button>
+        <button type="button" onClick={removeNumber}>Wait no</button>
+
+        </>
+    );
+}
+
+
+
+
+export default Phone;
