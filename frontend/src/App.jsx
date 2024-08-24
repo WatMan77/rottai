0f5dcaff494470c4d29cc06f3a8187225f9087ce
coodeex <juhani.s.lehto@gmail.com>
2024-08-24 14:27:41 +0300

    add vantaa button styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import {
   Page,
   Text,
@@ -19,45 +25,101 @@ import { H1 } from "./common/Text.jsx";
 import * as apiService from "./apiService.js";
 import * as SC from "./App.styles.js";
 
-
-
 function Home() {
   const [jobs, setJobs] = useState([]);
+  const navigate = useNavigate();
   const setOpenJobs = async () => {
-    setJobs(await apiService.getOpenJobs())
-  } 
+    setJobs(await apiService.getOpenJobs());
+  };
   useEffect(() => {
-    setOpenJobs()
-  }, [])
+    setOpenJobs();
+  }, []);
   return (
     <>
       <H1>Rott.AI</H1>
       <br />
       <br />
       <a href="https://toimistot.te-palvelut.fi/">
-      <img  src={Unemployment} />
+        <img src={Unemployment} />
       </a>
       {jobs && (
         <>
-          <h2 style={{color: "#000000"}}>Become a Vantaa Man!</h2>
+            <br />
+          <VantaaButton onClick={() => navigate("cv-form")}>
+            Become a Vantaa Man!
+            <br />
+          </VantaaButton>
+            <br />
+          <br />
           <SC.List>
-              {jobs.map((job, index) => {
-                return (
+            {jobs.map((job, index) => {
+              return (
                 <SC.ListItem key={index}>
-                  <h4><b>Position: {job.tyotehtava}</b></h4>
+                  <h4>
+                    <b>Position: {job.tyotehtava}</b>
+                  </h4>
                   <p>{job.organisaatio}</p>
-                  <p><b>Address:</b> {job.osoite}</p>
-                  <p><b>Application period ends:</b> {job.haku_paattyy_pvm}</p>
-                  <SC.LinkButton><SC.Link href={job.tyoavain}>Apply</SC.Link></SC.LinkButton>
+                  <p>
+                    <b>Address:</b> {job.osoite}
+                  </p>
+                  <p>
+                    <b>Application period ends:</b> {job.haku_paattyy_pvm}
+                  </p>
+                  <SC.LinkButton>
+                    <SC.Link href={job.tyoavain}>Apply</SC.Link>
+                  </SC.LinkButton>
                 </SC.ListItem>
-                )
-              })}
+              );
+            })}
           </SC.List>
+          <ul>
+            {jobs.map((job, index) => {
+              return (
+                <li key={index}>
+                  <h4>Position: {job.tyotehtava}</h4>
+                  <p>{job.organisaatio}</p>
+                  <a href={job.tyoavain}>Link</a>
+                  <p> Address: {job.osoite}</p>
+                  <p> Application period ends: {job.haku_paattyy_pvm}</p>
+                </li>
+              );
+            })}
+          </ul>
         </>
       )}
     </>
   );
 }
+
+const VantaaButton = styled.button`
+  --primary: #6a11cb;
+  --primaryHover: #2537fc;
+  background: linear-gradient(45deg, #6a11cb, var(--primary), #2575fc);
+  padding: 10px 20px;
+  border: none;
+  color: white;
+font-size: 3rem;
+  
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.03);
+    box-shadow: 0 6px 10px rgba(0, 0, 0, 0.15);
+    background: linear-gradient(45deg, #6a11cb, var(--primaryHover), #2575fc);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    transform: scale(1);
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    opacity: 0.9;
+  }
+`;
 
 const CVPage = () => {
   const styles = StyleSheet.create({
